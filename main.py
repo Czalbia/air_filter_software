6fdff139eb3bfa5fbb5db1edef2368fd2f21b3a0
Czalbia <jan.czalba@gmail.com>
2021-12-21 14:20:12 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+import json
+import time
+
+
+
+while True:
+
+    data= os.system("curl -X GET     --header 'Accept: application/json'     --header 'apikey: ykObTynCnnXkvdBM1o2ezfUmXKeRbyrM'     'https://airapi.airly.eu/v2/measurements/installation?installationId=3328' |json_pp > mydata.json")
+    pollution=[0,0,0,0,0,0]
+    f=open('mydata.json')
+    data = json.load(f)
+
+    counter =0
+    for i in data['current']['values']:
+        pollution[counter]+=i['value']
+        counter+=1
+    print(pollution)
+
+    if (pollution[0]>25 or pollution[1]>15 or pollution[2]>10):
+        print("Activate the filter!")
+
+    time.sleep(60*15)
+
+
+
+# curl -X GET -sD -     --compressed     --header 'Accept: application/json'     --header 'Accept-Encoding: gzip'     --header 'apikey: ykObTynCnnXkvdBM1o2ezfUmXKeRbyrM'     'https://airapi.airly.eu/v2/measurements/installation?installationId=3328'
+
+
